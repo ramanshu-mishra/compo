1b05a152beecc6a162653066d5e9bd422a4fde25
IndoPhoenix <vigyanavart@gmail.com>
2025-07-12 21:17:34 +0530

    little change

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 // import Card from "../components/Card1"
 // import Card2 from "../components/Card2" 
 // import Card3 from "../components/Card3"
-// import Card4 from "../components/Card4"
-import Card5 from "../components/Card5"
+import Card4 from "../components/Card4"
+// import Card5 from "../components/Card5"
 // import Card6 from "../components/Card6"
 // import Card7 from "../components/Card7"
 
@@ -10,7 +10,7 @@ import Card5 from "../components/Card5"
 export default async function Page(){
   return (
     <>
-    <Card5></Card5>
+    <Card4></Card4>
     </>
   )
 }
