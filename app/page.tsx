9fb4a578a2b0fcbe7720d2f14ccab3105b607b2a
IndoPhoenix <vigyanavart@gmail.com>
2025-07-12 19:04:36 +0530

    3d compo added

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@
 // import Card2 from "../components/Card2" 
 // import Card3 from "../components/Card3"
 // import Card4 from "../components/Card4"
-// import Card5 from "../components/Card5"
+import Card5 from "../components/Card5"
 // import Card6 from "../components/Card6"
-import Card7 from "../components/Card7"
+// import Card7 from "../components/Card7"
 
 
 export default async function Page(){
   return (
     <>
-    <Card7></Card7>
+    <Card5></Card5>
     </>
   )
 }
